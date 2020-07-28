021dd6e9d50ddb65b41c0389868c150ea39768ee
althof <althoframdhan3@gmail.com>
2020-07-28 21:26:46 +0700

    Adding Google API Auth for login

--- app.js
+++ app.js
@@ -7,13 +7,23 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose')
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
 const app = express();
 
+// Static file in public folder
 app.use(express.static('public'));
+
+// allow .ejs to html templating
 app.set('view engine', 'ejs');
+
+// req.body.name
 app.use(bodyparser.urlencoded({
     extended: true
 }));
+
+// SESSION
 app.use(session({
     secret: 'Our little secret',
     resave: false,
@@ -22,32 +32,71 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
+// server MongoDB
 mongoose.connect('mongodb://localhost:27017/userDB', {
     useNewUrlParser: true
 });
 mongoose.set('useCreateIndex', true);
 
-
+// scheme
 const user = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
+
+// scheme PLUGIN
 user.plugin(passportLocalMongoose);
+user.plugin(findOrCreate);
 
 // Models User
 const User = new mongoose.model('User', user)
 
-// Use Passport to 
+// Use Passport
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user,done) => {
+    done(null, user.id)
+});
+passport.deserializeUser((id,done) => {
+    User.findById(id, (err,user) => {
+        done(err,user);
+    })
+});
+
+// Google Auth
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://127.0.0.1:3000/auth/google/secrets",
+        passReqToCallback   : true
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+        User.findOrCreate({
+            googleId: profile.id
+        }, function (err, user) {
+            return done(err, user);
+        });
+    }
+));
 
 // HOME
 app.route('/')
     .get((req, res) => {
         res.render('home');
     })
+
+// GOOGLE login
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: [ 'https://www.googleapis.com/auth/userinfo.profile',
+      , 'https://www.googleapis.com/auth/userinfo.email' ]
+    }));
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', {
+        successRedirect: '/secrets',
+        failureRedirect: '/login'
+    }));
 
 // LOGIN
 app.route('/login')
@@ -62,13 +111,13 @@ app.route('/login')
         });
 
         req.logIn(user, (err) => {
-            if(err){
+            if (err) {
                 alert(err);
             } else {
-                passport.authenticate('local')(req,res,() => {
+                passport.authenticate('local')(req, res, () => {
                     res.redirect('/secrets');
                 });
-            }            
+            }
         })
     })
 
@@ -78,12 +127,14 @@ app.route('/register')
         res.render('register');
     })
     .post((req, res) => {
-        User.register({username: req.body.username}, req.body.password, (err,user) => {
+        User.register({
+            username: req.body.username
+        }, req.body.password, (err, user) => {
             console.log(user);
-            if(err){
+            if (err) {
                 res.redirect('/register');
             } else {
-                passport.authenticate('local')(req,res, () => {
+                passport.authenticate('local')(req, res, () => {
                     res.redirect('/secrets');
                 });
             }
@@ -100,7 +151,7 @@ app.route('/logout')
 // SECRETS
 app.route('/secrets')
     .get((req, res) => {
-        if(req.isAuthenticated()){
+        if (req.isAuthenticated()) {
             res.render('secrets');
         } else {
             res.redirect('/login');
